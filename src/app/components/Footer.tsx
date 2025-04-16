419e0ac81dcc1088476d30bff001aa15e192c7d1
Andrii Fedorenko <andrii.fedorenko@onedata.de>
2025-04-16 08:05:15 +0200

    footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -19,7 +19,7 @@ export default function Footer() {
             <p className="text-sm max-w-sm">
               {t('about')}
             </p>
-            <div className="flex space-x-4 mt-2">
+            {/* <div className="flex space-x-4 mt-2">
               <a href="https://twitter.com" className="brutalist-border p-2" aria-label="Twitter">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                   <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"></path>
@@ -37,7 +37,7 @@ export default function Footer() {
                   <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
                 </svg>
               </a>
-            </div>
+            </div> */}
          
           </div>
           
